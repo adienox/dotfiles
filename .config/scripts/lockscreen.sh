c0b0841f0761788c661ec1d7aff859e1745fea03
AdwaitAdk <adwaitadk@tuta.io>
2022-03-20 14:20:02 +0545

    Increased delay for locking the screen

--- .config/scripts/lockscreen.sh
+++ .config/scripts/lockscreen.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Only exported variables can be used within the timer's command.
-export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
-
 # Run xidlehook
 xidlehook \
   `# Don't lock when there's a fullscreen application` \
@@ -13,8 +10,8 @@ xidlehook \
   --timer 60 \
     '$HOME/.config/scripts/lockFade.sh fade'\
     '$HOME/.config/scripts/lockFade.sh restore'\
-  `# Undim & lock after 15 more seconds` \
-  --timer 15 \
+  `# Undim & lock after 60 more seconds` \
+  --timer 60 \
     '$HOME/.config/scripts/lockFade.sh restore; betterlockscreen -l' \
     '' \
   `# Finally, suspend an hour after it locks` \
